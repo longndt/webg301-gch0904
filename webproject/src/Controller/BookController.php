12aa28e8b9c433f02812584cce987cd57247d3e7
longndt <55429399+longndt@users.noreply.github.com>
2021-12-16 12:18:34 +0700

    16122021

--- webproject/src/Controller/BookController.php
+++ webproject/src/Controller/BookController.php
@@ -4,20 +4,85 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
-   /**
+    /**
+    * @Route("/getbooklist", methods="GET", name="book_list_api")
+    */
+    public function getBookList() : JsonResponse {
+        $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+        return $this->json(['books' => $books], 200);
+    }
+   
+    /**
     * @Route("/books", name="book_index")
     */
-    public function showBookList() {
+    public function bookIndex() {
         $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
         return $this->render("book/index.html.twig",
         [
             'books' => $books
-        ]
-    );
+        ]);
+    }
+
+    /**
+    * @Route("/book/list", name="book_list")
+    */
+    public function bookList() {
+        $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+        return $this->render("book/list.html.twig",
+        [
+            'books' => $books
+        ]);
+    }
+
+    /**
+     * @Route("/book/{id}", name="book_detail")
+     */
+    public function bookDetail($id) {
+        $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+        if ($book == null) {
+            $this->addFlash("Error", "Book not exist");
+            return $this->redirectToRoute("book_index");
+        }
+        return $this->render("book/detail.html.twig",
+        [
+            'book' => $book
+        ]);
+    }
+
+    /** 
+     * @Route("/book/delete/{id}", name="book_delete")
+     */
+    public function bookDelete($id) {
+        $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+        if ($book == null) {
+            $this->addFlash("Error", "Book delete failed");  
+        } else {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($book);
+            $manager->flush();
+            $this->addFlash("Success", "Book delete succeed"); 
+        }
+        return $this->redirectToRoute("book_index");
+    }
+
+    /**
+     * @Route("/book/add", name="book_add")
+     */
+    public function bookAdd(Request $request) {
+
+    }
+
+    /**
+     * @Route("/book/edit/{id}", name="book_edit")
+     */
+    public function bookEdit(Request $request, $id) {
+
     }
 
 }
